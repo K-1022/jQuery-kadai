62b500c45ff52f7d867d6fe07988fbb26ef6a8b9
K-1022 <kikunagaryu2004@gmail.com>
2024-06-23 12:38:32 +0900

    応用問題4

--- 応用問題1/script.js
+++ 応用問題1/script.js
@@ -1,6 +1,45 @@
 $(function () {
+  //応用1のコメント
+  // $("#btn").on("click", function () {
+  //   $("ul").append("<li>" + $('[id="text-box"]').val() + "</li>");
+  //   $("#text-box").val("");
+  // });
+  let text = "#text-box";
+  let error = "#error";
   $("#btn").on("click", function () {
-    $("ul").append("<li>" + $('[id="text-box"]').val() + "</li>");
-    $("#text-box").val("");
+    $(text).val();
+    if ($(text).val() === "") {
+      $(text).addClass("sample");
+      $(error).text("入力必須です");
+      $(error).css("color", "red");
+    } else {
+      $("ul").append("<li>" + $(text).val() + "</li>");
+      $(text).val("");
+      $(error).text("");
+      $(text).removeClass("sample");
+    }
+  });
+  $(text).on("input", function () {
+    if ($(text).val().length >= 20) {
+      $(text).addClass("sample");
+      $(error).text("20 文字以上は入力できません");
+      $(error).css("color", "red");
+      $("#btn").prop("disabled", true);
+    } else {
+      $(error).text("");
+      $(text).removeClass("sample");
+      $("#btn").prop("disabled", false);
+    }
   });
 });
+
+//応用3の問題
+// やることが入力されずに追加ボタン押下された場合下記を行う
+//   - リストに追加しない
+//   - 入力フォームの背景を赤にする
+//   - 入力フォームの下に、「入力必須です」と表示する
+//応用4の問題
+// - やることが 20 文字以上入力されたら、（追加ボタンを押していなくても）下記を行う
+//   - 入力フォーム背景を赤にする
+//   - 入力フォームの下に、「20 文字以上は入力できません」と表示する
+//   - 追加ボタンを押せないようにする
